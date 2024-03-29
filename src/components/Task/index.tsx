c56b5e0afd5300ca97782724a0cfe6ab69e6be95
Lucas barros <fallenagamesytb@gmail.com>
2024-03-28 21:19:07 -0300

    feat: Added useState hooks to buttons and implemented the functionality to swap between them

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,16 +1,37 @@
+import { useState } from 'react'
 import * as S from './styles'
 
-const Task = () => (
-  <S.Card>
-    <S.Title>Nome da tarefa</S.Title>
-    <S.Tag>Importante</S.Tag>
-    <S.Tag>Pendente</S.Tag>
-    <S.Description />
-    <S.ActionsBar>
-      <S.Buttons>Editar</S.Buttons>
-      <S.Buttons>Remover</S.Buttons>
-    </S.ActionsBar>
-  </S.Card>
-)
+type Props = {
+  title: string
+  priority: string
+  status: string
+  description: string
+}
+
+const Task = ({ description, status, title, priority }: Props) => {
+  const [isEdit, setEdit] = useState(false)
+
+  return (
+    <S.Card>
+      <S.Title>{title}</S.Title>
+      <S.Tag>{priority}</S.Tag>
+      <S.Tag>{status}</S.Tag>
+      <S.Description value={description} />
+      <S.ActionsBar>
+        {isEdit ? (
+          <>
+            <S.Buttons>Salvar</S.Buttons>
+            <S.Buttons onClick={() => setEdit(false)}>Cancelar</S.Buttons>
+          </>
+        ) : (
+          <>
+            <S.Buttons onClick={() => setEdit(true)}>Editar</S.Buttons>
+            <S.Buttons>Remover</S.Buttons>
+          </>
+        )}
+      </S.ActionsBar>
+    </S.Card>
+  )
+}
 
 export default Task
--- src/containers/TasksList/index.tsx
+++ src/containers/TasksList/index.tsx
@@ -1,25 +1,42 @@
 import Task from '../../components/Task'
 import { ContainerTL } from './styles'
 
+const tasks = [
+  {
+    title: 'Estudar Typescript',
+    description: 'Verificar o fork feito de aulas typescript da github',
+    priority: 'importante',
+    status: 'Pendente'
+  },
+  {
+    title: 'Estudar Python',
+    description:
+      'Continuar curso na Udemy para aprimorar conhecimento de Python',
+    priority: 'importante',
+    status: 'Pendente'
+  },
+  {
+    title: 'Treinar academia',
+    description: 'academia todos os dias da semana e basquete aos sabados',
+    priority: 'importante',
+    status: 'Pendente'
+  }
+]
+
 const TasksList = () => (
   <ContainerTL>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
     <ul>
-      <li>
-        <Task />
-      </li>
-      <li>
-        <Task />
-      </li>
-      <li>
-        <Task />
-      </li>
-      <li>
-        <Task />
-      </li>
-      <li>
-        <Task />
-      </li>
+      {tasks.map((t) => (
+        <li key={t.title}>
+          <Task
+            description={t.description}
+            title={t.title}
+            status={t.status}
+            priority={t.priority}
+          />
+        </li>
+      ))}
     </ul>
   </ContainerTL>
 )
